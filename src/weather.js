73ca09f9bf7bfbba98d3aa9db3d26d4a0499ca89
joanacfs <73188818+joanacfs@users.noreply.github.com>
2020-12-15 22:46:11 +0100

    added forecast API and started forecast

--- src/weather.js
+++ src/weather.js
@@ -9,14 +9,14 @@ function isToday() {
     let month = months[now.getMonth()];
     return (today.innerHTML = `${day}, ${dayMonth} ${month}`);
 }
-///CHOOSE A CITY
+///SEARCH A CITY
 function citySearch(event) {
     event.preventDefault();
     let input = document.querySelector("#search-input");
     let chosenCity = input.value;
     let key = "a34d18380688cbcca8e36a7c0180b644";
-    let cityNameUrl = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${key}&units=metric`;
-    axios.get(cityNameUrl).then(displayWeather);
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${chosenCity}&appid=${key}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
 }
 
 let form = document.querySelector("#search-engine");
@@ -24,23 +24,31 @@ form.addEventListener("submit", citySearch);
 
 // /// OR SEARCH CURRENT POSITION
 let currentLocation = document.querySelector("#AtualLocationButton");
-currentLocation.addEventListener("click", determineLocation);
+currentLocation.addEventListener("click", determineBrowserLocation);
 
-function determineLocation() {
+function determineBrowserLocation() {
     let geolocation = navigator.geolocation.getCurrentPosition(showPosition);
 }
 
+window.onload = function() {
+    determineBrowserLocation();
+}
+
 function showPosition(position) {
-    let actualCity = document.querySelector("#search-engine");
-    actualCity.innerHTML = "Atual Location";
     let latitude = `${position.coords.latitude}`;
     console.log(latitude);
     let longitude = `${position.coords.longitude}`;
     console.log(longitude);
+    let actualCity = document.querySelector("#search-input");
+    actualCity.innerHTML = "Atual Location";
     let key = "a34d18380688cbcca8e36a7c0180b644";
     let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric`;
     axios.get(url).then(displayWeather);
     console.log("url  " + url);
+
+    url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&daily={part}&appid=${key}&units=metric`;
+    axios.get(url).then(displayForecast);
+    console.log(url);
 }
 //  ACTUAL WEATHER
 
@@ -73,13 +81,28 @@ function displayWeather(response) {
     return (wDescription, wSpeed, wHumidity, temperatureMax, temperatureMin, weatherIcon);
 }
 
+function displayForecast(response) {
+    let forecast = document.querySelector("#whole-forecast");
+    console.log(response.data.daily);
+    forecast.innerHTML =
+        `<div class = "col-2">
+        <div id="monday">
+        Monday
+        </div> 
+        <img src = "#" alt = "img1" class = " col-11 image"/>
+        <div class = "Tempe">
+        <strong> 18 </strong> 14º 
+        </div> 
+    </div>`;
+}
+
 ///ºF AND ºC
 function toFahrenheit(event) {
     event.preventDefault
     let tempFahrenheitMax = document.querySelector("#temp-now-max");
     let tempFahrenheitMin = document.querySelector("#temp-now-min");
     tempFahrenheitMax.innerHTML = Math.round(celsiusMaxTemp * 9 / 5 + 32);
-    tempFahrenheitMin.innerHTML = (celsiusMinTemp * 9 / 5 + 32);
+    tempFahrenheitMin.innerHTML = Math.round(celsiusMinTemp * 9 / 5 + 32);
 }
 
 
